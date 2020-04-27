036043b79a552ed0f790e81c3fb3b87375bcd4ca
David Tyrpak <tyrpak@usc.edu>
2020-04-27 12:19:58 -0700

    Update Permutation.java
    client code is done. need to test with input

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -1,8 +1,30 @@
+import java.util.Iterator;
+
+import edu.princeton.cs.algs4.StdIn;
 
 public class Permutation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//k is the number of items to sample
+		int k = Integer.parseInt(args[0]);
+		
+		RandomizedQueue<String> q = new RandomizedQueue<String>();
+		
+		while (!StdIn.isEmpty())
+		{
+			
+			String item = StdIn.readString();
+			
+			q.enqueue(item);
+			
+		}
+		
+	 Iterator<String> iterator = q.iterator();
+	 
+	 int i = 0;
+	 
+	 while (i <= k) System.out.println(iterator.hasNext());
 
 	}
 
