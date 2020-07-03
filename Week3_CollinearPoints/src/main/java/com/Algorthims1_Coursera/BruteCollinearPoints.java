f6e469dac3c95e32534e4414c24e52a0bdf94266
David Tyrpak <tyrpak@usc.edu>
2020-07-03 13:05:11 -0700

    Update BruteCollinearPoints.java
    added private method fourTuples which returns all unique tuple subsets of size four .
    Tomorrow I'll need to provide checks to ensure that redundent entires arent added, for example 1,1,2,3,4 should really be seen as 1,2,3,4

--- Week3_CollinearPoints/src/main/java/com/Algorthims1_Coursera/BruteCollinearPoints.java
+++ Week3_CollinearPoints/src/main/java/com/Algorthims1_Coursera/BruteCollinearPoints.java
@@ -17,11 +17,12 @@ public class BruteCollinearPoints {
 		
 	}
 	
-	public int numberOfSegments() { // the number of line segments
+	//public int numberOfSegments() { // the number of line segments
 	
-		return segments().length;
+		
+		//return segments().length;
 	
-	}
+	//}
 	
 	
 	//is the given point inside a given array of points
@@ -35,22 +36,93 @@ public class BruteCollinearPoints {
 		return false;
 		}
 	
+	//are two points collinear
+	private boolean isCollinear(Point a, Point b) {
+		
+		if ( a.compareTo(b) == 0 ) return true;
+		
+		else 
+			return false;
+	}
 	
 	
-	 public LineSegment[] segments() { // the line segments
+	//finds all tuples of size four
+	private ArrayList<ArrayList<Point>> fourTuples() {
+		
+		ArrayList<Point> innerList = new ArrayList<Point>();
+		
+		ArrayList<ArrayList<Point>> outerList = new ArrayList<ArrayList<Point>>();
+		
+		for (int i = 0; i < points.length; i ++) {
+			
+			
+			
+			
+			
+			 for (int j = i+1; j < points.length; j++){
+				 
+				 
+				 
+				 
+				 for (int k = j+1; k < points.length; k++) {
+					 
+					 
+					 for (int z = k + 1; z < points.length; z++) {
+				 
+					 
+					 innerList = new ArrayList<Point>();
+				 
+					 innerList.add(points[i]);
+				 	 
+				 	 innerList.add(points[j]);
+				 	 
+					 innerList.add(points[k]);
+					 
+					 innerList.add(points[z]);
+					 
+					 outerList.add(innerList);
+					 }
+				 }
+						 
+			 }
+			 }
+		return outerList;
+		}
+		
+
+	
+	
+	
+	
+	 //public LineSegment[] segments() { // the line segments
 	  
+		 //ArrayList<LineSegment> str = new ArrayList<LineSegment>();
+				 
 		 
 		 //need to convert to type Array
 		 
-		 LineSegment[] arrayLineSegments = lineSegments.toArray(new LineSegment[lineSegments.size()]);
+		 //LineSegment[] arrayLineSegments = lineSegments.toArray(new LineSegment[lineSegments.size()]);
 	 
-	 }
+	 //}
 	 
 	 
 		 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BruteCollinearPoints bcp = new Brute
+		
+		Point a = new Point(0,0);
+        Point b = new Point(1,1);
+        Point c = new Point(1,3);
+        Point d = new Point(1,1);
+        Point e = new Point(2,5);
+        
+        Point[] array = new Point[] {a,b,c,d,e};
+        
+		BruteCollinearPoints bcp = new BruteCollinearPoints(array);
+		
+		bcp.fourTuples();
+		
+		System.out.println(Arrays.deepToString(bcp.fourTuples().toArray()));
 
 	}
 
